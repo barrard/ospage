a158ff59c1bc347706eae303e04030fc96c669b1
David <barrard@gmail.com>
2017-10-22 01:44:09 -1000

    sick CPU colors change

--- www/js/js.js
+++ www/js/js.js
@@ -4,6 +4,28 @@ function init(){
 	get_user_ip_address()
 }
 
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+function red_or_green(core_MHz, core_id){
+	var core_color = ~~core_MHz.map(1400, 3500, 0, 510)
+
+	if(core_color < 255){
+		// console.log('This should be green-yellow')
+		// console.log('core_color = '+core_color)
+		$('#cpu_core_'+core_id).css('background', 'rgb('+core_color+',255 , 0)')
+	}else{
+		// console.log('This should be yellow-red')
+		// console.log('core_color = '+core_color)
+		core_color-=255
+
+		$('#cpu_core_'+core_id).css('background', 'rgb(255 ,'+ (255-core_color) +', 0)')
+		// console.log('this should be yellow'+ (255-core_color))
+	}
+}
+
+
 function handle_err(err){
 	console.log('err handler called');
 	console.log(err);
@@ -56,9 +78,22 @@ var active_cpu_watch = false
 var cpu_graphic_made = false
 var timerInterval
 
+function remove_cpu_inacitve_class(){
+	Array.prototype.forEach.call($('.cpu_core_square'), function(i){
+		$(i).removeClass('cpu_core_inactive')
+	})
+}
+
+function add_cpu_inacitve_class(){
+	Array.prototype.forEach.call($('.cpu_core_square'), function(i){
+		$(i).addClass('cpu_core_inactive')
+	})
+}
+
 $('#watch_cpu_btn').on('click', function(e){
 	if(!active_cpu_watch){
 		console.log('lets observe the CPU')
+		remove_cpu_inacitve_class()
 
 		clearInterval(timerInterval);
 
@@ -70,13 +105,34 @@ $('#watch_cpu_btn').on('click', function(e){
 		active_cpu_watch = true
 	}else{
 		console.log('lets stop watching the CPU')
+		add_cpu_inacitve_class()
 
 		clearInterval(timerInterval);
 		$(this).removeClass('btn-danger')
-		$(this).addClass('btn-info').html('CPU watch')
+		$(this).addClass('btn-info').html('watch CPU cores')
 		active_cpu_watch = false
 	}
 })
+
+//makes boxes for the cores
+
+function make_cpu_squares(cores){
+	for (var i = 0; i < cores+1; i++) {
+		console.log('i');
+		var core = document.createElement('div');
+		core.id="cpu_core_"+i
+		core.classList.add('cpu_core_square')
+		core.classList.add('cpu_core_inactive')
+		core.innerHTML=i+1
+		if(i===cores){
+			core.innerHTML='Avg.'
+			core.id="cpu_core_avg"
+		} 
+		$('#cpu_cores_container').append(core);
+	}
+}
+
+
 function handle_get_cpu_data_success(data){
 	var main_CPU_data_array = [
 		'Architecture',
@@ -97,19 +153,62 @@ function handle_get_cpu_data_success(data){
 	data.data.forEach((i)=>{
 		// console.log(i.split(':'))
 		var line = i.split(':')
+		if(line[0]==='CPU(s)'){
+			console.log('cores is '+line[1])
+			make_cpu_squares(parseInt(line[1]))
+		}
 		if(main_CPU_data_array.indexOf(line[0])!=-1){
 			console.log(line[0])
 			//append this data in the order its found is crazy but works
 			//run the functiong to append to the table
 			//tr id = "main_CPU_data"
+			var head = line[0]
+			var th = "<th>"+head+"</th>"
+			$('#main_CPU_head').append(th)
 			var data = line[1]
+			//fix numers like 3500.0000
+			if(!isNaN(data)) data = Math.floor(data)
 			var td = "<td>"+data+"</td>"
 			$('#main_CPU_data').append(td)
 		}
 	})
 }
 function handle_get_cpu_frequency_success(data){
-	console.log(data)
+	var cores_array = data.data.slice(0, -1)
+	var core_avg=0;
+	var core_count = 0
+	console.log(cores_array)
+	var core_id
+	var core_MHz = 0
+	cores_array.forEach((i, ind)=>{
+
+		if(ind%2===0){
+			//processor line, slip on :
+			core_id = parseInt(i.split(':')[1])
+			console.log('mod 2 === true')
+			console.log(core_id)
+		}else{
+			core_count++
+			//core MHz line
+			core_MHz = parseInt(i.split(':')[1])
+			console.log('core mhz is '+core_MHz)
+			// console.log('THE STARTING CORE_COLOR IS '+core_color)
+			red_or_green(core_MHz, core_id)
+
+			core_avg+=core_MHz
+		}
+
+		console.log('cpu_core_'+core_id+' should be MHZ '+core_MHz)
+		console.log('maps too '+~~core_MHz.map(1400, 3500, 0, 510))
+
+
+
+//  this needs to be incremented last
+	})
+	core_avg /=core_count
+	//          ~~ === Math.floor()
+	console.log(~~core_avg)
+	red_or_green(~~core_avg,'avg')
 }
 function get_cpu_frequency(){
 	$.ajax('/get_cpu_frequency',{
@@ -119,6 +218,7 @@ function get_cpu_frequency(){
 
 }
 function CPU_grapic_make(){
+	cpu_graphic_made = true
 	// make a call to the system to get CPU data
 	$.ajax('/get_cpu_data',{
 		success:handle_get_cpu_data_success,
